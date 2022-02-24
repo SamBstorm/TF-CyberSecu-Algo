9228d82e13e1d530b122684f1a59b76df760894e
Legrain Samuel <samuel.legrain@bstorm.be>
2022-02-24 13:25:22 +0100

    Ajout correction : Tri d'un tableau

---
+++ TF-CyberSecu-Algo.ExoTableauTri/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace TF_CyberSecu_Algo.ExoTableauTri
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int TAILLE = 7;
+            int[] tableau = new int[] { 3,2,1,4,5,6,7 };
+
+            bool havePermutation = true;
+            for (int i = 0; i < TAILLE - 1 && havePermutation; i++)
+            {
+                havePermutation = false; //Contrôle si une permutation à été effectué, si pas de parmutation = tableau trié
+                //PASS
+                for (int j = i + 1; j < TAILLE; j++)
+                {
+                    if (tableau[i] >= tableau[j])
+                    {
+                        int temp = tableau[j];
+                        tableau[j] = tableau[i];
+                        tableau[i] = temp;
+                        havePermutation = true;
+                    }
+                }
+            }
+            //Array.Sort(tableau);
+
+            foreach (int value in tableau)
+            {
+                Console.WriteLine(value);
+            }
+        }
+    }
+}
