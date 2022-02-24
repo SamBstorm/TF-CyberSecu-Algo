c80e8c575d179e21347a7ecdb5fbbcf0f748be0d
Legrain Samuel <samuel.legrain@bstorm.be>
2022-02-24 17:05:06 +0100

    Add Correction : Tableau Pion

---
+++ TF-CyberSecu-Algo.ExoTableauPion/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace TF_CyberSecu_Algo.ExoTableauPion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool[] tab = new bool[10];
+            int positionPion = 0;
+            tab[positionPion] = true;
+
+            foreach (bool cell in tab)
+            {
+                if (cell) Console.Write("©");
+                else Console.Write("_");
+            }
+
+            ConsoleKeyInfo touche = Console.ReadKey(true);
+            while (touche.Key != ConsoleKey.Q)
+            {
+                if (touche.Key == ConsoleKey.G && positionPion > 0) {
+                    tab[positionPion] = !(tab[positionPion]);
+                    positionPion--; //positionPion = (positionPion == 0)? tab.Length -1 : positionPion-1;
+                    tab[positionPion] = !(tab[positionPion]);
+                }
+                else if (touche.Key == ConsoleKey.D && positionPion < tab.Length - 1)
+                {
+                    tab[positionPion] = !(tab[positionPion]);
+                    positionPion++;
+                    tab[positionPion] = !(tab[positionPion]);
+                }
+                Console.Clear();
+                foreach (bool cell in tab)
+                {
+                    if (cell) Console.Write("©");
+                    else Console.Write("_");
+                }
+                touche = Console.ReadKey(true);
+            }
+
+        }
+    }
+}
