5bdb91d1aa8f0acfd280e38274a2fee7f7c264b8
Legrain Samuel <samuel.legrain@bstorm.be>
2022-02-24 15:40:01 +0100

    Add Demo : Console.ReadKey() & Console.Clear()

---
+++ TF-CyberSecu-Algo.DemoConsole/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace TF_CyberSecu_Algo.DemoConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string test = Console.ReadLine();
+            //Console.WriteLine($"Vous avez écrit : {test}");
+            
+            ConsoleKeyInfo touche;
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("Veuillez appuyez sur 'A' :");
+                touche = Console.ReadKey(true);
+                Console.WriteLine($"Vous avez écrit : {touche.Key}");
+            } while (touche.Key != ConsoleKey.A);
+            Console.Clear();
+            Console.WriteLine("Merci d'avoir appuyez sur 'A'...");
+        }
+    }
+}
