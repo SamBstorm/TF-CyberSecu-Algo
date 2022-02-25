28ec9156ee03bff6046dabe557285212c902595c
Legrain Samuel <samuel.legrain@bstorm.be>
2022-02-25 17:06:30 +0100

    Add démos : Procédures

---
+++ TF-CyberSecu-Algo.DemoSousProgVarGlobal/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace TF_CyberSecu_Algo.DemoSousProgVarGlobal
+{
+    class Program
+    {
+        static string message = "Hello World!";     //Variable globale : A éviter (sauf une fois l'Orienté objet vu ;) )
+        static void Main(string[] args)
+        {
+            string nomDeUtilisateur = "Sam";
+            nomDeUtilisateur = MettreEnMajuscule(nomDeUtilisateur);
+            DireBonjour(nomDeUtilisateur, false);
+            int chiffre = 5;
+            chiffre = MultiplierPar2(chiffre);
+            Console.WriteLine(chiffre);
+        }
+
+        public static void DireBonjour(string name, bool estUneFemme) {
+            Console.WriteLine($"{BonjourMonsieurMadame(estUneFemme)} {name}!");
+        }
+
+
+        public static string MettreEnMajuscule(string text)
+        {
+            string result = text.ToUpper(); //ToUpper() met le texte qui l'appelle en majuscule
+            return result;
+        }
+
+        public static string BonjourMonsieurMadame(bool isFemale)
+        {
+            /*
+            string result;
+            if (isFemale) result = "Bonjour Madame";
+            else result = "Bonjour Monsieur";
+            return result;*/
+            if (isFemale) return "Bonjour Madame";
+            else return "Bonjour Monsieur";
+        }
+
+        public static int MultiplierPar2(int x)
+        {
+            return x * 2;
+        }
+    }
+}
---
+++ TF-CyberSecu-Algo.DemoTableauRef/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace TF_CyberSecu_Algo.DemoTableauRef
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] t1 = new int[] { 1, 2, 3, 4 };    //Référence, donc une copie de l'adresse mémoire
+            int[] t2 = t1;                          //Donc il s'agit pas d'une copie des valeurs, mais bien un nouveau lien vers la même valeur.
+
+            t1[0] = 5;
+            showArray(t1);
+            showArray(t2);
+        }
+
+        public static void showArray(int[] array)
+        {
+            foreach (int number in array)
+            {
+                Console.Write($"{number} ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
