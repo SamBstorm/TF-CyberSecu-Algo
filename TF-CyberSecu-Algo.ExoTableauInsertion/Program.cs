3e9b63e588ac647ef0aa0402ef0ab63497a7008e
Legrain Samuel <samuel.legrain@bstorm.be>
2022-02-25 15:48:46 +0100

    Add corrections : Tableau insertion et Suppression

---
+++ TF-CyberSecu-Algo.ExoTableauInsertion/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace TF_CyberSecu_Algo.ExoTableauInsertion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] tab = new int[] { 1,2,3,5,6,7 };
+
+            int value;
+            do
+            {
+                Console.WriteLine("Veuillez indiquer un nombre : ");
+            } while (!int.TryParse(Console.ReadLine(),out value));
+
+            int position = tab.Length;
+            bool NotFindIndex = true;
+            for (int i = 0; i < tab.Length && NotFindIndex; i++)
+            {
+                if (value < tab[i]) { 
+                    position = i; 
+                    NotFindIndex = false; 
+                }
+            }
+
+            for (int i = position; i < tab.Length; i++)
+            {
+                int temp = tab[i];
+                tab[i] = value;
+                value = temp;
+            }
+
+            foreach (int number in tab)
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
---
+++ TF-CyberSecu-Algo.ExoTableauSuppression/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace TF_CyberSecu_Algo.ExoTableauSuppression
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] tableau = new int[] { 3,3,2,5,6,7 };
+            int temp, count = 0;
+            int value;
+            do
+            {
+                Console.WriteLine("Veuillez indiquer le nombre à supprimer :");
+            } while (!int.TryParse(Console.ReadLine(), out value));
+
+            for (int i = 0; i < tableau.Length-count; i++)
+            {
+                if(value == tableau[i])
+                {
+                    tableau[i] = 0;
+                    for (int j = i; j < tableau.Length-1; j++)
+                    {
+                        temp = tableau[j + 1];
+                        tableau[j + 1] = tableau[j];
+                        tableau[j] = temp;
+                    }
+                    i--;
+                    count++;
+                }
+                
+            }
+
+            foreach (int number in tableau)
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
